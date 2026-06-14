22561fd0038e8c60e86a36704da7dd462b7b9737
Kiran Namawar <kirannamawar@gmail.com>
2026-06-14 15:34:58 +0530

    add styles to room form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,40 @@ import { useNavigate } from "react-router";
 function App() {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center">
-      <form
-        onSubmit={(evt) => {
-          const formData = new FormData(evt.target);
-          const room = formData.get("room") as string;
-          if (!room.trim()) return;
-          navigate(`/${room}`);
-        }}
+    <div className="relative h-screen">
+      <div className="flex flex-col items-center justify-center h-full">
+        <form
+          className="flex flex-col items-center border rounded p-4"
+          onSubmit={(evt) => {
+            const formData = new FormData(evt.target);
+            const room = formData.get("room") as string;
+            if (!room.trim()) return;
+            navigate(`/${room}`);
+          }}
+        >
+          <label htmlFor="room" className="self-start">
+            Room Name
+          </label>
+          <input
+            id="room"
+            type="text"
+            title="room"
+            className="border rounded p-2 mb-2"
+          />
+          <button
+            type="submit"
+            className="border rounded px-4 py-2 cursor-pointer hover:bg-green-400 bg-green-300  dark:text-black"
+          >
+            Join
+          </button>
+        </form>
+      </div>
+      <a
+        href="https://github.com/KiranNamawar/demo-webrtc"
+        className="absolute bottom-4 left-1/2 -translate-x-1/2 text-blue-500"
       >
-        <label htmlFor="room">Room Name</label>
-        <input id="room" type="text" title="room" />
-        <button type="submit">Join</button>
-      </form>
+        Source Code
+      </a>
     </div>
   );
 }
